028eaafb9ff625ec48c6bec224f211f0a4efeab5
hasnattanvir <hasnattanvir014@gmail.com>
2022-03-06 12:04:58 +0600

    author data update

--- resources/views/layouts/website.blade.php
+++ resources/views/layouts/website.blade.php
@@ -69,23 +69,22 @@
     <div class="site-footer">
       <div class="container">
         <div class="row mb-5">
-          <div class="col-md-4">
+          <div class="col-md-3">
             <h3 class="footer-heading mb-4">About Us</h3>
-            {{-- <p>{{$setting->description}}</p> --}}
+            <p>{{$settings->about_site}}</p>
           </div>
-          <div class="col-md-3 ml-auto">
+          <div class="col-md-4 ml-auto">
             <!-- <h3 class="footer-heading mb-4">Navigation</h3> -->
             <ul class="list-unstyled float-left mr-5">
-              <li><a href="#">About Us</a></li>
-              <li><a href="#">Advertise</a></li>
-              <li><a href="#">Careers</a></li>
-              <li><a href="#">Subscribes</a></li>
+              <li><a href="{{route('home')}}">Home</a></li>
+              <li><a href="{{route('website.about')}}">About</a></li>
+              <li><a href="{{route('website.contact')}}">Contact</a></li>
+              {{-- <li><a href="#">{{route('website')}}</a></li> --}}
             </ul>
             <ul class="list-unstyled float-left">
-              <li><a href="#">Travel</a></li>
-              <li><a href="#">Lifestyle</a></li>
-              <li><a href="#">Sports</a></li>
-              <li><a href="#">Nature</a></li>
+               @foreach ($categories as $item)
+                  <li><a href="{{route('website.category',['slug'=> $item->slug])}}">{{$item->name}}</a></li>
+                @endforeach
             </ul>
           </div>
           <div class="col-md-4">
@@ -94,11 +93,11 @@
             <div>
               <h3 class="footer-heading mb-4">Connect With Us</h3>
               <p>
-                <a href="#"><span class="icon-facebook pt-2 pr-2 pb-2 pl-0"></span></a>
-                <a href="#"><span class="icon-twitter p-2"></span></a>
-                <a href="#"><span class="icon-instagram p-2"></span></a>
-                <a href="#"><span class="icon-rss p-2"></span></a>
-                <a href="#"><span class="icon-envelope p-2"></span></a>
+                <a href="{{$settings->facebook}}" target="_blank"><span class="icon-facebook pt-2 pr-2 pb-2 pl-0"></span></a>
+                <a href="{{$settings->twitter}}" target="_blank"><span class="icon-twitter p-2"></span></a>
+                <a href="{{$settings->instagram}}" target="_blank"><span class="icon-instagram p-2"></span></a>
+                <a href="{{$settings->reddit}}" target="_blank"><span class="icon-rss p-2"></span></a>
+                <a href="{{$settings->email}}" target="_blank"><span class="icon-envelope p-2"></span></a>
               </p>
             </div>
           </div>
@@ -106,10 +105,8 @@
         <div class="row">
           <div class="col-12 text-center">
             <p>
-              <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
-              Copyright &copy; <script>document.write(new Date().getFullYear());</script> All rights reserved | This template is made with <i class="icon-heart text-danger" aria-hidden="true"></i> by <a href="https://colorlib.com" target="_blank" >Colorlib---</a>Downloaded from <a href="https://themeslab.org/" target="_blank">Themeslab</a>
-              <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
-              </p>
+               {{$settings->copyright}}
+            </p>
           </div>
         </div>
       </div>
